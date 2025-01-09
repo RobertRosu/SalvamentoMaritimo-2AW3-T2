852cc3989923f6dd8d7231c5e9fc7b551e5c2484
RobertRosu <i_rosu@fpzornotza.com>
2025-01-09 09:42:07 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'SMH-2AW3-T2';
+}
