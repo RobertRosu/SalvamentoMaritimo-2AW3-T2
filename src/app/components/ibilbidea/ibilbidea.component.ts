020aabf675e1080eac86a42fee71147031595d3f
AnderPati <a_patino@fpzornotza.com>
2025-01-14 21:31:43 +0100

    pagina ibilbidea
    diseño de la pagina acabado, y funcionalidad mediante array integrado

--- src/app/components/ibilbidea/ibilbidea.component.ts
+++ src/app/components/ibilbidea/ibilbidea.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./ibilbidea.component.scss']
 })
 export class IbilbideaComponent {
-
+  events = [
+    {
+      date: 'fsg',
+      title: 'Ibilbide 4',
+      description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima deleniti, rerum ducimus assumenda optio nulla, quia obcaecati velit incidunt eum nisi odio officia itaque nam ea repellendus consequatur, voluptatum suscipit.'
+    },
+    {
+      date: 'dfgdg',
+      title: 'Ibilbide 3',
+      description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima deleniti, rerum ducimus assumenda optio nulla, quia obcaecati velit incidunt eum nisi odio officia itaque nam ea repellendus consequatur, voluptatum suscipit.'
+    },
+    {
+      date: '2:30 - 4:00pm',
+      title: 'Ibilbide 2',
+      description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima deleniti, rerum ducimus assumenda optio nulla, quia obcaecati velit incidunt eum nisi odio officia itaque nam ea repellendus consequatur, voluptatum suscipit.'
+    },
+    {
+      date: '5:00 - 8:00pm',
+      title: 'Ibilbide 1',
+      description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima deleniti, rerum ducimus assumenda optio nulla, quia obcaecati velit incidunt eum nisi odio officia itaque nam ea repellendus consequatur, voluptatum suscipit.'
+    }
+  ];
 }
