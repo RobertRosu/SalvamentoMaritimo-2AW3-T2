bae6008aea34e91fdf6ee869c0657b79b2cc543d
Jon Zabala <j_zabalabur@fpzornotza.com>
2025-01-14 13:55:21 +0100

    Formularioarekin jarraitzen

--- resources/views/rescuedPeople/form.blade.php
+++ resources/views/rescuedPeople/form.blade.php
@@ -7,52 +7,83 @@
     <h4 class="mt-4 text-dark">Kargatzen...</h4>
 @stop
 @section('content')
-<!-- Formulario de edición -->
-<form action="{{ route('erreskatatuak.update', ['erreskatatuak' => $rescuedPerson->id]) }}" method="POST" enctype="multipart/form-data">
-    @csrf
-    @method('PUT')
+<div class="container">
+    <div class="row">
+        <!-- Ezkerreko kolumna: Editable formularioa -->
+        <div class="col-md-6">
+            <form action="{{ route('erreskatatuak.update', ['erreskatatuak' => $rescuedPerson->id]) }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                @method('PUT')
 
-    <!-- Campo Nombre -->
-    <div class="form-group">
-        <label for="name">Izena</label>
-        <input type="text" class="form-control" id="name" name="name" value="{{ $rescuedPerson->name }}" required>
-    </div>
+                <!-- Izena -->
+                <div class="form-group">
+                    <label for="name">Izena</label>
+                    <input type="text" class="form-control" id="name" name="name" value="{{ $rescuedPerson->name }}" required>
+                </div>
 
-    <!-- Campo País -->
-    <div class="form-group">
-        <label for="country">Herrialdea</label>
-        <input type="text" class="form-control" id="country" name="country" value="{{ $rescuedPerson->country }}" required>
-    </div>
+                <!-- Herrialdea -->
+                <div class="form-group">
+                    <label for="country">Herrialdea</label>
+                    <input type="text" class="form-control" id="country" name="country" value="{{ $rescuedPerson->country }}" required>
+                </div>
 
-    <!-- Campo Género -->
-    <div class="form-group">
-        <label for="genre">Sexua</label>
-        <input type="text" class="form-control" id="genre" name="genre" value="{{ $rescuedPerson->genre }}" required>
-    </div>
+                <!-- Generoa -->
+                <div class="form-group">
+                    <label for="genre">Generoa</label>
+                    <input type="text" class="form-control" id="genre" name="genre" value="{{ $rescuedPerson->genre }}" required>
+                </div>
 
-    <!-- Campo Fecha de nacimiento -->
-    <div class="form-group">
-        <label for="birth_date">Jaiotze-data</label>
-        <input type="date" class="form-control" id="birth_date" name="birth_date" value="{{ \Carbon\Carbon::parse($rescuedPerson->birth_date)->toDateString() }}" required>
+                <!-- Jaiotze Data -->
+                <div class="form-group">
+                    <label for="birth_date">Jaiotze Data</label>
+                    <input type="date" class="form-control" id="birth_date" name="birth_date" value="{{ $rescuedPerson->birth_date }}" required>
+                </div>
+
+                <!-- Diagnostikoa -->
+                <div class="form-group">
+                    <label for="diagnostic">Diagnostikoa</label>
+                    <input type="text" class="form-control" id="diagnostic" name="diagnostic" value="{{ $rescuedPerson->diagnostic }}" required>
+                </div>
+
+                <!-- argazkia -->
+                <div class="form-group">
+                    <label for="photo_src">Argazkia</label>
+                    <input type="file" class="form-control-file" id="photo_src" name="photo_src">
+                    @if ($rescuedPerson->photo_src)
+                        <img src="{{ asset('storage/' . $rescuedPerson->photo_src) }}" alt="argazkia" width="100">
+                    @endif
+                </div>
+
+                <!-- Aldaketa gordetzeko botoia -->
+                <button type="submit" class="btn btn-primary">Aldatu</button>
+            </form>
         </div>
 
-    <!-- Campo Diagnóstico -->
-    <div class="form-group">
-        <label for="diagnostic">Diagnostikoa</label>
-        <input type="text" class="form-control" id="diagnostic" name="diagnostic" value="{{ $rescuedPerson->diagnostic }}" required>
-    </div>
+        <!-- Eskuineko kolumna: Irudi eta atributuak ikusteko (ez editable) -->
+        <div class="col-md-6">
+            <!-- argazkia -->
+            <div class="form-group">
+                <label for="photo">Argazkia</label>
+                @if ($rescuedPerson->photo_src)
+                    <img src="{{ asset('storage/img/' . $rescuedPerson->photo_src) }}" alt="argazkia" width="200">
+                @else
+                    <p>No hay foto disponible</p>
+                @endif
+            </div>
 
-    <!-- Campo Foto -->
-    <div class="form-group">
-        <label for="photo_src">Argazkia</label>
-        <input type="file" class="form-control-file" id="photo_src" name="photo_src">
-        @if ($rescuedPerson->photo_src)
-            <img src="{{ asset('storage/' . $rescuedPerson->photo_src) }}" alt="Foto actual" width="100">
-        @endif
-    </div>
+            <!-- Rescue ID (Ez aldagarria) -->
+            <div class="form-group">
+                <label for="rescue_id">Rescue ID</label>
+                <input type="text" class="form-control" id="rescue_id" name="rescue_id" value="{{ $rescuedPerson->rescue_id }}" readonly>
+            </div>
 
-    <!-- Botón para enviar el formulario y guardar los cambios -->
-    <button type="submit" class="btn btn-primary">Aldaketak gorde</button>
-</form>
+            <!-- Doctor ID (Ez aldagarria) -->
+            <div class="form-group">
+                <label for="doctor_id">Doctor ID</label>
+                <input type="text" class="form-control" id="doctor_id" name="doctor_id" value="{{ $rescuedPerson->doctor_id }}" readonly>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
