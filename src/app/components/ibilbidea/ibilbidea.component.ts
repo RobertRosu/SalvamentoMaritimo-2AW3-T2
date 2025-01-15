cf6f819a00ae3645a7549eebd3c75b299670c345
AnderPati <a_patino@fpzornotza.com>
2025-01-15 11:05:17 +0100

    footer y detalles de ibilbidea
    la etiqueta que añadía el componente del footer se ha cambiado de los componentes al archivo html de app.component general

--- src/app/components/ibilbidea/ibilbidea.component.ts
+++ src/app/components/ibilbidea/ibilbidea.component.ts
@@ -8,24 +8,24 @@ import { Component } from '@angular/core';
 export class IbilbideaComponent {
   events = [
     {
-      date: 'fsg',
+      date: '16:00 - 22:30',
       title: 'Ibilbide 4',
       description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima deleniti, rerum ducimus assumenda optio nulla, quia obcaecati velit incidunt eum nisi odio officia itaque nam ea repellendus consequatur, voluptatum suscipit.'
     },
     {
-      date: 'dfgdg',
+      date: '5:15 - 13:25',
       title: 'Ibilbide 3',
       description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima deleniti, rerum ducimus assumenda optio nulla, quia obcaecati velit incidunt eum nisi odio officia itaque nam ea repellendus consequatur, voluptatum suscipit.'
     },
     {
-      date: '2:30 - 4:00pm',
+      date: '14:30 - 17:30',
       title: 'Ibilbide 2',
       description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima deleniti, rerum ducimus assumenda optio nulla, quia obcaecati velit incidunt eum nisi odio officia itaque nam ea repellendus consequatur, voluptatum suscipit.'
     },
     {
-      date: '5:00 - 8:00pm',
+      date: '5:00 - 12:00',
       title: 'Ibilbide 1',
       description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima deleniti, rerum ducimus assumenda optio nulla, quia obcaecati velit incidunt eum nisi odio officia itaque nam ea repellendus consequatur, voluptatum suscipit.'
-    }
+    },
   ];
 }
