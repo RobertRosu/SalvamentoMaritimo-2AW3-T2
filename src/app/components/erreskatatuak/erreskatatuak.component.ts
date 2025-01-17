a72b8551c5b98f1f349ce485e4d6087a711a14ba
AnderPati <a_patino@fpzornotza.com>
2025-01-17 08:58:28 +0100

    login con token acabado

--- src/app/components/erreskatatuak/erreskatatuak.component.ts
+++ src/app/components/erreskatatuak/erreskatatuak.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 import { Errefuxiatua } from 'src/app/models/Errefuxiatua';
 
 @Component({
@@ -6,7 +6,7 @@ import { Errefuxiatua } from 'src/app/models/Errefuxiatua';
   templateUrl: './erreskatatuak.component.html',
   styleUrls: ['./erreskatatuak.component.scss']
 })
-export class ErreskatatuakComponent {
+export class ErreskatatuakComponent implements OnInit{
   errefuxiatuak: Errefuxiatua[] = [
     new Errefuxiatua(1, 'Errefuxiatu 1', 16, 'gizona', 'nigeria', ''),
     new Errefuxiatua(2, 'Errefuxiatu 2', 17, 'emakumea', 'maroko', ''),
@@ -34,6 +34,42 @@ export class ErreskatatuakComponent {
       adina: 0,
       naziotasuna: '0',
     };
+  }
+
+  password: string = '';
+  isAuthenticated: boolean = false;
+  private authTokenKey = 'authToken';
+  showError: boolean = false;
+
+  ngOnInit() {
+    // Verificar si el token generado está en sessionStorage
+    const storedToken = sessionStorage.getItem(this.authTokenKey);
+    this.isAuthenticated = !!storedToken; // Si hay un token, se considera autenticado
+  }
+
+  validatePassword() {
+    const correctPassword = 'password'; // Cambia esto por tu contraseña
+    if (this.password === correctPassword) {
+      this.isAuthenticated = true;
+      const authToken = this.generateRandomToken();
+      sessionStorage.setItem(this.authTokenKey, authToken); // Guardar token en sessionStorage
+      this.password = '';
+      this.showError = false;
+    } else {
+      this.showError = true;
+    }
+  }
+
+  logout() {
+    this.isAuthenticated = false;
+    sessionStorage.removeItem(this.authTokenKey); // Eliminar token al cerrar sesión
+  }
+
+  private generateRandomToken(): string {
+    // Genera un token aleatorio utilizando la API nativa de crypto
+    return Array.from(crypto.getRandomValues(new Uint8Array(32)))
+      .map(byte => byte.toString(16).padStart(2, '0'))
+      .join('');
   }
   
 }
